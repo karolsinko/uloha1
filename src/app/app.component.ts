06a87ee2b18239b19b715f0c1c5aa8945c999d36
karolsinko <karol.sinko30@gmail.com>
2022-02-18 17:33:10 +0100

    kontrola checkboxov + zaciatok bin hodnôt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {mergeWith} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'uloha1';
+  cenaKs: number = 0;
+  mnozstvo: number = 0;
+  spolu: number = 0;
+
+
 }
